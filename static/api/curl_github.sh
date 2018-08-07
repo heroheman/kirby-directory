5e81aef53f18da989514827830d5ff71e78c741e
Florenz Heldermann <hi@flore.nz>
2018-08-07 18:29:02 +0200

    curl order

--- static/api/curl_github.sh
+++ static/api/curl_github.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
 for (( counter=1; counter<=6; counter+=1 )); do
-    curl -vs "https://api.github.com/search/issues?q=is:open+repo:jenstornell/kirby-plugins&sort=updated&order=asc&per_page=100&page=$counter"| jq -r '.items' > tmp/page-$counter.json
+    curl -vs "https://api.github.com/search/issues?q=is:open+repo:jenstornell/kirby-plugins&sort=updated&order=desc&per_page=100&page=$counter"| jq -r '.items' > tmp/page-$counter.json
 done
 
 jq -s '[.[][]]' tmp/page-*.json > items.json
