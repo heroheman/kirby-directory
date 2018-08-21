8b2924858bc283640cc24af970775770ebd69dca
Florenz Heldermann <hi@flore.nz>
2018-08-22 00:22:21 +0200

    fix curl

--- static/api/curl_github.sh
+++ static/api/curl_github.sh
@@ -4,6 +4,8 @@ for (( counter=1; counter<=6; counter+=1 )); do
     curl -vs "https://api.github.com/search/issues?q=is:open+repo:jenstornell/kirby-plugins&sort=updated&order=desc&per_page=100&page=$counter"| jq -r '.items' > tmp/page-$counter.json
 done
 
+rm items.json
+
 jq -s '[.[][]]' tmp/page-*.json > items.json
 
 rm tmp/page-*
