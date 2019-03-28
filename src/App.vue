c579beb2b0f7f2351fb167cfb5855267c066c554
Florenz Heldermann <hi@flore.nz>
2019-03-28 22:42:45 +0100

    remove console.log

--- src/App.vue
+++ src/App.vue
@@ -44,7 +44,6 @@ export default {
   methods: {
     searchVisibilityChanged (searchVisible, entry) {
       this.searchVisible = searchVisible
-      console.log(searchVisible)
     }
   },
   metaInfo () {
